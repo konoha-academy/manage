d50f2fe528e79f1a79af049402e5611a08f0867a
zeasy <jozeyuri@gmailcom>
2021-09-08 13:48:40 -0300

    update wrapper dockerfile

--- wrapper/server/app.ts
+++ wrapper/server/app.ts
@@ -3,6 +3,8 @@ import path from "path";
 // Boot express
 export const app: Application = express();
 
+app.use("/static", express.static(__dirname + "/static"));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
